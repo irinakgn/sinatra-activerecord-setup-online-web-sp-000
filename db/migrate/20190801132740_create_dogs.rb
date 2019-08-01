549d6787ce822488efd85c795406efc9bea227e5
Irina Kagan <irinagamb@gmail.com>
2019-08-01 13:34:50 +0000

    Automatically backed up by Learn

--- db/migrate/20190801132740_create_dogs.rb
+++ db/migrate/20190801132740_create_dogs.rb
@@ -1,7 +1,8 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
-  def up
-   end
-
-   def down
-   end
+  def change
+      create_table :dogs do |t|
+        t.string :name
+        t.string :breed
+      end
+    end
 end
